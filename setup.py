3ca8570cd1b236e7f15439203793936529c18328
joni <you@example.com>
2025-06-16 19:55:04 +0200

    fixed dependencies in pip package

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 project_folder = "cave"
 with open('README.md') as f:
     long_desc = f.read()
-with open("requirements.txt") as f:
-    dependencies = map(lambda x: x.replace("\n",""), f.readlines())
 
 setup(
     name='cave-infra',
@@ -14,7 +12,11 @@ setup(
     description='Automation toolkit for automated provisioning virtual infrastructure.',
     long_description=long_desc,
     long_description_content_type="text/markdown",
-    install_requires=dependencies,
+    install_requires=[
+        "Jinja2==3.1.6",
+        "libvirt-python==11.4.0",
+        "MarkupSafe==3.0.2"
+    ]
     url='https://github.com/sn0ja/cave/',
     author='sn0ja',
 )
